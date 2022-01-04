41edfdacf25c0132c49b9ca6f54303811743515d
Mohammad-muneeb <muneebsheikh323@gmail.com>
2022-01-04 01:37:25 -0800

    initial changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,28 @@
 import 'package:flutter/material.dart';
 void main(){
   runApp(MaterialApp(
-    title: "Muneeb app"
-    //home: Container()
+    title: "Myapp",
+    home: HomePage(),
   ));
+}
+
+class HomePage extends StatelessWidget {
+  HomePage ({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Muneeb's App"),
+        ),
+
+        body: Container(
+          child: Center(child: Text("Hi Adil Bitch!")),
+          ),
+      ),
+
+
+    );
+  }
 }
