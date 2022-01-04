ebfb70cf515a5a28bdd3c35fe715a6d37e6ea43c
Mohammad-muneeb <muneebsheikh323@gmail.com>
2022-01-04 02:01:55 -0800

    Chapter 2

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main(){
 }
 
 class HomePage extends StatelessWidget {
-  HomePage ({Key? key}) : super(key: key);
+ // HomePage ({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -15,13 +15,19 @@ class HomePage extends StatelessWidget {
       child: Scaffold(
         appBar: AppBar(
           title: Text("Muneeb's App"),
-        ),
-
-        body: Center(
+          ),
+          body: Center(
           child: Container(
             color: Colors.green,
             width: 100,
-            height: 100,))
+            height: 100,
+            child: Text("FLUTTER",
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.red, 
+            ),),
+            ))
+
       ),
 
 
